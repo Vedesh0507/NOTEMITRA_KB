8cfafabe892992fc8703da38d8bf8f751d842649
Vedesh0507 <ManepalliPavanVedesh@mictech.edu.in>
2026-02-07 01:49:42 +0530

    Fix Vercel build: add standalone output and trace ignores

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: ['lh3.googleusercontent.com', 'notemitra-pdfs.s3.amazonaws.com', 'res.cloudinary.com'],
+  },
+  experimental: {
+    outputFileTracingIgnores: ['**canvas**', '**pdfjs-dist**'],
   },
 };
 
